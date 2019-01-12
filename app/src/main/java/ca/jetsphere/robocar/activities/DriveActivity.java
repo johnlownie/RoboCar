dc678637c39ef95c3064d37db21c0ce8cb7fdb4b
John Lownie <jlownie@gmail.com>
2019-01-12 14:18:19 -0500

    Added example messenger service

--- app/src/main/java/ca/jetsphere/robocar/activities/DriveActivity.java
+++ app/src/main/java/ca/jetsphere/robocar/activities/DriveActivity.java
@@ -1,7 +1,14 @@
 package ca.jetsphere.robocar.activities;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.ActivityInfo;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,9 +18,11 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import ca.jetsphere.robocar.R;
 import ca.jetsphere.robocar.devices.Joystick;
+import ca.jetsphere.robocar.services.MessengerService;
 
 /**
  *
@@ -21,6 +30,11 @@ import ca.jetsphere.robocar.devices.Joystick;
 public class DriveActivity extends AppCompatActivity
 {
     private static String TAG = "DriveActivity";
+
+    /** Messenger for communicating with the service. */
+    Messenger mService = null;
+    /** Flag indicating whether we have called bind on the service. */
+    boolean mBound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +46,12 @@ public class DriveActivity extends AppCompatActivity
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
         setContentView(R.layout.activity_drive);
+
+        final TextView textView1 = findViewById(R.id.textView1);
+        final TextView textView2 = findViewById(R.id.textView2);
+        final TextView textView3 = findViewById(R.id.textView3);
+        final TextView textView4 = findViewById(R.id.textView4);
+        final TextView textView5 = findViewById(R.id.textView5);
 
         final RelativeLayout rlJoystick = findViewById(R.id.layout_joystick);
         final Joystick joystick = new Joystick(getApplicationContext(), rlJoystick, R.drawable.joystick_button);
@@ -48,6 +68,12 @@ public class DriveActivity extends AppCompatActivity
                 if(arg1.getAction() == MotionEvent.ACTION_DOWN || arg1.getAction() == MotionEvent.ACTION_MOVE) {
                     int direction = joystick.get8Direction();
                     Log.i(TAG, "Direction: " + direction);
+
+                    textView1.setText("X: " + String.valueOf(joystick.getX()));
+                    textView2.setText("Y: " + String.valueOf(joystick.getY()));
+                    textView3.setText("Angle: " + String.valueOf(joystick.getAngle()));
+                    textView4.setText("Distance: " + String.valueOf(joystick.getDistance()));
+                    textView5.setText("Direction: " + direction);
                 }
                 return true;
             }
@@ -57,11 +83,61 @@ public class DriveActivity extends AppCompatActivity
         btnTrack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Log.i(TAG, "BT Connect clicked...");
+                sayHello();
 
-                Intent myIntent = new Intent(DriveActivity.this, MainActivity.class);
-                DriveActivity.this.startActivity(myIntent);
+//                Intent myIntent = new Intent(DriveActivity.this, MainActivity.class);
+//                DriveActivity.this.startActivity(myIntent);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to the service
+        bindService(new Intent(this, MessengerService.class), mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Unbind from the service
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+
+    /**
+     * Class for interacting with the main interface of the service.
+     */
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // This is called when the connection with the service has been
+            // established, giving us the object we can use to
+            // interact with the service.  We are communicating with the
+            // service using a Messenger, so here we get a client-side
+            // representation of that from the raw IBinder object.
+            mService = new Messenger(service);
+            mBound = true;
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been
+            // unexpectedly disconnected -- that is, its process crashed.
+            mService = null;
+            mBound = false;
+        }
+    };
+
+    public void sayHello() {
+        if (!mBound) return;
+        // Create and send a message to the service, using a supported 'what' value
+        Message msg = Message.obtain(null, MessengerService.MSG_SAY_HELLO, 0, 0);
+        try {
+            mService.send(msg);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/ca/jetsphere/robocar/services/MessengerService.java
@@ -0,0 +1,53 @@
+package ca.jetsphere.robocar.services;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.widget.Toast;
+
+public class MessengerService extends Service
+{
+    static public final int MSG_SAY_HELLO = 1;
+
+    /**
+     * Target we publish for clients to send messages to IncomingHandler.
+     */
+    Messenger mMessenger;
+
+    /**
+     * Handler of incoming messages from clients.
+     */
+    static class IncomingHandler extends Handler {
+        private Context applicationContext;
+
+        IncomingHandler(Context context) {
+            applicationContext = context.getApplicationContext();
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_SAY_HELLO:
+                    Toast.makeText(applicationContext, "hello!", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    /**
+     * When binding to the service, we return an interface to our messenger
+     * for sending messages to the service.
+     */
+    @Override
+    public IBinder onBind(Intent intent) {
+        Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_SHORT).show();
+        mMessenger = new Messenger(new IncomingHandler(this));
+        return mMessenger.getBinder();
+    }
+}
